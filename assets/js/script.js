38fdd62f83b87c989689d2802676ff76e9417658
Ranjith M <126071064+Ranjith-M7@users.noreply.github.com>
2024-03-09 20:02:01 +0530

    Added the condition for error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+const specialChars = ["+", "-", "*", "/", "%", "="];
 let output = "";
 
 // Define a function to calculate based on the button clicked
 const calculate = (btnValue) => {
-  if (output !== "" && btnValue === "=") {
+  if (output === "Error") {
+    output = "";
+  } else if (btnValue === "=" && specialChars.includes(output.slice(-1))) {
+    output = "Error";
+  } else if (output !== "" && btnValue === "=") {
     output = eval(output);
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
     output = output.toString().slice(0, -1);
   } else {
+    if (output === "" && btnValue === "=") {
+      return;
+    }
     output += btnValue;
   }
 
